3d114a3b9e0df86224aebaf79bb0cf49a7ff39fb
WqyJh <781345688@qq.com>
2024-05-11 20:20:34 +0800

    Add ChanHandler

---
+++ handler.go
@@ -0,0 +1,15 @@
+package etcdseq
+
+type ChanHandler struct {
+	ch chan<- Info
+}
+
+func NewChanHandler(ch chan Info) *ChanHandler {
+	return &ChanHandler{
+		ch: ch,
+	}
+}
+
+func (h *ChanHandler) OnChange(info Info) {
+	h.ch <- info
+}
